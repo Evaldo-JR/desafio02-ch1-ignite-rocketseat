e2f783ab5b384a635fa8ed991e52a6142132af48
Evaldo Junior <evaldo_jux@hotmail.com>
2022-06-04 19:48:19 -0300

    [Challenge Complete] Concluindo o desafio
    O desafio consistia em dividir a função de mostrar filmes em 2 componentes (SideBar, Content) para que toda a responsabilidade não fique na raíz da aplicação. Assim, cada um tem sua função de alterar os estados e realizar a busca na API para retornar os resultados necessários para cada gênero de filme.
    - Um assunto importante que aprendi nesse desafio foi sobre [Elevando o State](https://pt-br.reactjs.org/docs/lifting-state-up.html)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+import { Button } from './Button';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type SideBarProps = {
+  selectedGenreId: number;
+  onClickGenre: (id: number) => void;
+};
+
+export function SideBar({ selectedGenreId, onClickGenre }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
